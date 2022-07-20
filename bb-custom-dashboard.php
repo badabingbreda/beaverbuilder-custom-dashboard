4e43d92310cd160358fb1fd53179ee1807e31a6c
badabingbreda <didou@badabing.nl>
2022-07-20 22:26:22 +0200

    changed to all public static class functions

--- bb-custom-dashboard.php
+++ bb-custom-dashboard.php
@@ -3,7 +3,7 @@
  * Plugin Name: Beaver Builder Custom Dashboard
  * Plugin URI: http://www.bluedogdigitalmarketing.com
  * Description: Customize the WordPress Dashboard using a Beaver Builder Template
- * Version: 0.5
+ * Version: 0.6.0
  * Author: Blue Dog Digital
  * Author URI: http://www.bluedogdigitalmarketing.com
  * License: GPL2
@@ -15,4 +15,12 @@ if ( ! defined('WPINC') ) {
 	die;
 }
 
+define( 'BBCUSTOMDASHBOARD_VERSION', '0.6.0' );
+define( 'BBCUSTOMDASHBOARD_DIR', plugin_dir_path( __FILE__ ) );
+define( 'BBCUSTOMDASHBOARD_FILE', __FILE__ );
+define( 'BBCUSTOMDASHBOARD_URL', plugins_url( '/', __FILE__ ) );
+
+
 require('classes/class-bb-custom-dashboard.php');
+
+new BBCustomDashboard();
--- classes/class-bb-custom-dashboard.php
+++ classes/class-bb-custom-dashboard.php
@@ -5,75 +5,82 @@ if ( ! defined('WPINC') ) {
 	die;
 }
 
-add_action( 'plugins_loaded', array( BB_Custom_Dashboard::get_instance(), 'init' ), 99 );
 
-class BB_Custom_Dashboard {
+class BBCustomDashboard {
+
+	// slug for the bb template to look for, change if already taken by another post/page or cpt
+	protected static $dashboard_slug = 'dashboard';
 	
-	protected static $instance = NULL;
-    public static function get_instance() {
-        if ( null === self::$instance ) {
-            self::$instance = new self;
-        }
-        return self::$instance; 
-    }
-	
-	public function init() {
+	public function __construct() {
 		
-		// remove dashboard metaboxes
-		add_action( 'wp_dashboard_setup', array( $this, 'remove_dashboard_meta' ), 99 );
+		// remove dashboard metaboxes by running late 999
+		add_action( 'wp_dashboard_setup', __CLASS__ .  '::remove_dashboard_meta' , 999 );
 		
 		// enqueue layout styles and scripts in admin
 		add_action( 'admin_enqueue_scripts', 'FLBuilder::register_layout_styles_scripts' );
 		add_action( 'admin_enqueue_scripts', 'FLBuilder::enqueue_all_layouts_styles_scripts' );
 		
 		// enqueue custom styles and scripts in admin
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_styles_scripts' ) );
+		add_action( 'admin_enqueue_scripts', __CLASS__ .  '::enqueue_styles_scripts'  );
 		
 		// display layout
-		add_action( 'admin_footer', array( $this, 'display_dashboard_layout' ) );
-    }
-	
-	/**
-	 * Remove ALL dashboard widgets
-	 * 
-	 */
-	function remove_dashboard_meta() {
-		
-		global $wp_meta_boxes;
-		
-		$wp_meta_boxes = array( 'dashboard' => array(
-			'advanced' => array(),
-			'side' => array(),
-			'normal' => array(),
-		) );
+		add_action( 'admin_footer', __CLASS__ . '::display_dashboard_layout' );
+
 	}
 	
 	/**
-	 * Enqueue custom styles and scripts in admin
-	 * 
+	 * remove_dashboard_meta
+	 *
+	 * @return void
 	 */
-	function enqueue_styles_scripts() {
+	public static function remove_dashboard_meta() {
 		
-		wp_register_style( 'bbcd-styles', plugins_url( 'assets/css/bbcd-admin.css', dirname(__FILE__) ), array(), '' );
-		wp_register_script( 'bbcd-js', plugins_url( 'assets/js/bbcd-admin.js', dirname(__FILE__) ), array(), '' );
+		global $wpdb;
+		// test if our slug exists, otherwise return early
+		$post_if = $wpdb->get_var("SELECT count(post_title) FROM $wpdb->posts WHERE post_name like '" . self::$dashboard_slug . "'");
+		
+		if ( !$post_if) return;
+
+		global $wp_meta_boxes;
+
+		// reset the wp_meta_boxes array to just our bb-dashboard
+		$wp_meta_boxes = array( 
+			'bb-dashboard' => array(
+				'advanced' => array(),
+				'side' => array(),
+				'normal' => array(),
+			) 
+		);
+	}
+		
+	/**
+	 * enqueue_styles_scripts
+	 *
+	 * @return void
+	 */
+	public static function enqueue_styles_scripts() {
+		wp_register_style( 'bbcd-styles', BBCUSTOMDASHBOARD_URL . 'assets/css/bbcd-admin.css', array() , BBCUSTOMDASHBOARD_VERSION, 'all' );
+		wp_register_script( 'bbcd-js',  BBCUSTOMDASHBOARD_URL . 'assets/js/bbcd-admin.js', array(), BBCUSTOMDASHBOARD_VERSION );
 		
 		wp_enqueue_style( 'bbcd-styles' );
 		wp_enqueue_script( 'bbcd-js' );
 	}
-	
+		
 	/**
-	 * Display dashboard layout
-	 * 
+	 * display_dashboard_layout
+	 *
+	 * @return void
 	 */
-	function display_dashboard_layout() {
+	public static function display_dashboard_layout() {
 
+		// show when visiting backend dashboard only
 		if ( get_current_screen()->base !== 'dashboard' ) {
 			return;
 		}
 
 		$layout = 	'<div id="bbcd-layout" class="welcome-panel" style="display: none;">';
 		$layout .= 		'<div class="welcome-panel-content">';
-		$layout .= 			do_shortcode( '[fl_builder_insert_layout slug="dashboard" type="fl-builder-template"]' );
+		$layout .= 			\do_shortcode( '[fl_builder_insert_layout slug="' . self::$dashboard_slug . '" type="fl-builder-template"]' );
 		$layout .= 		'</div>';
 		$layout .= 	'</div>';
 
